14794f7de22fdeb5414159a0b6a83bcf31a1b3c6
ev0lution <36345644+ev0lution@users.noreply.github.com>
2018-03-13 20:59:26 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,18 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+    if (message.content === 'help') {
+    	message.reply('Hello, I am Kodiak. Developed by @Ev0!');
+        
+        client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+        
+        client.on('message', message => {
+    if (message.content === 'commands') {
+    	message.reply('My commands are help and commands.');
+        
+        client.on('message', message => {
     if (message.content === 'hi') {
     	message.reply('pickle');
        

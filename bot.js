31db2501a38a6e8dcf256ef0a19beabe510252d5
ev0lution <36345644+ev0lution@users.noreply.github.com>
2018-03-13 21:11:41 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,15 +5,9 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-bot.on("message", function(msg) {
-    if(msg.content === prefix + "!help"
-       msg.channel.send("Hello, I'm Kodiak (Developed by @Ev0), and I like to help you with things!")
-    
-
 client.on('message', message => {
     if (message.content === 'help') {
     	message.reply('Hello, I am Kodiak. Developed by @Ev0!');
-      
   	}
 });
 

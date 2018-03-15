c72c064f649680b9fdb71237351f8e5cab8d58b9
ev0lution <36345644+ev0lution@users.noreply.github.com>
2018-03-14 23:36:16 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,14 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'help') {
-    	message.reply('Hello, I am Kodiak. Developed by @Ev0!');
-       
-  	}
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login('token');

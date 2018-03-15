57dc477e3d351f0ae73dae7029c48eb062e81bb6
ev0lution <36345644+ev0lution@users.noreply.github.com>
2018-03-14 23:45:49 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,24 +8,7 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'help') {
     	message.reply('Hello, I am Kodiak. Developed by @Ev0!');
- 
-      client.on ("guildMemberAdd", member=> {
-
-    let welcomeGuild = member.guild; 
-
-    let welcomeChannel = welcomeGuild.channels.find("name", "welcome");
-
-    welcomeChannel.send(`Welcome to Billy's Dev Server, ${member} I hope you have fun!`);
-
-});
-client.on ("guildMemberRemove", member=> {
-
-  let welcomeGuild = member.guild; 
-
-  let welcomeChannel = welcomeGuild.channels.find("name", "welcome");
-
-  welcomeChannel.send(`Sorry to see you leave., ${member}`);
- 
+       
   	}
 });
 

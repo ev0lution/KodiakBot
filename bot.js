db72e2fd6a949bb958ab4998f78664b63404ac19
ev0lution <36345644+ev0lution@users.noreply.github.com>
2018-03-14 23:45:00 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,21 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
+    console.log('I am ready!');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
-client.on ("guildMemberAdd", member=> {
+client.on('message', message => {
+    if (message.content === 'help') {
+    	message.reply('Hello, I am Kodiak. Developed by @Ev0!');
+ 
+      client.on ("guildMemberAdd", member=> {
 
     let welcomeGuild = member.guild; 
 
     let welcomeChannel = welcomeGuild.channels.find("name", "welcome");
 
-    welcomeChannel.send(Welcome to Billy's Dev Server, ${member} I hope you have fun!);
+    welcomeChannel.send(`Welcome to Billy's Dev Server, ${member} I hope you have fun!`);
 
 });
 client.on ("guildMemberRemove", member=> {
@@ -26,9 +24,10 @@ client.on ("guildMemberRemove", member=> {
 
   let welcomeChannel = welcomeGuild.channels.find("name", "welcome");
 
-  welcomeChannel.send(Sorry to see you leave., ${member});
+  welcomeChannel.send(`Sorry to see you leave., ${member}`);
+ 
+  	}
 });
 
-
-});
-client.login('NDEyMDM1ODAwNzU1MzM5Mjg2.DWJXxA.8DzKXelHDmr3XyneplzyO5jXYYM');
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);

de76aa3a33b2a55a559ed241f06dba3e67bf54cc
ev0lution <36345644+ev0lution@users.noreply.github.com>
2018-03-14 23:39:00 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(Logged in as ${client.user.tag}!);
 });
 
 client.on('message', msg => {
@@ -11,4 +11,24 @@ client.on('message', msg => {
   }
 });
 
-client.login('token');
+client.on ("guildMemberAdd", member=> {
+
+    let welcomeGuild = member.guild; 
+
+    let welcomeChannel = welcomeGuild.channels.find("name", "welcome");
+
+    welcomeChannel.send(Welcome to Billy's Dev Server, ${member} I hope you have fun!);
+
+});
+client.on ("guildMemberRemove", member=> {
+
+  let welcomeGuild = member.guild; 
+
+  let welcomeChannel = welcomeGuild.channels.find("name", "welcome");
+
+  welcomeChannel.send(Sorry to see you leave., ${member});
+});
+
+
+});
+client.login('NDEyMDM1ODAwNzU1MzM5Mjg2.DWJXxA.8DzKXelHDmr3XyneplzyO5jXYYM');
